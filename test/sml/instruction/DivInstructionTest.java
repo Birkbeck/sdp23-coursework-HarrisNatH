43bf8873bc715425bb4c563094c1a8988040d182
Harris <harris.hendra@gmail.com>
2023-02-20 21:57:03 +0000

    DivInstruction class and test

---
+++ test/sml/instruction/DivInstructionTest.java
@@ -0,0 +1,58 @@
+package sml.instruction;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import static sml.Registers.Register.*;
+
+class DivInstructionTest {
+  private Machine machine;
+  private Registers registers;
+
+  @BeforeEach
+  void setUp() {
+    machine = new Machine(new Registers());
+    registers = machine.getRegisters();
+    //...
+  }
+
+  @AfterEach
+  void tearDown() {
+    machine = null;
+    registers = null;
+  }
+
+  @Test
+  void executeValid() {
+    registers.set(EAX, 10);
+    registers.set(EBX, 5);
+    Instruction instruction = new DivInstruction(null, EAX, EBX);
+    instruction.execute(machine);
+    Assertions.assertEquals(2, machine.getRegisters().get(EAX));
+  }
+
+  @Test
+  void executeValidTwo() {
+    registers.set(EAX, 0);
+    registers.set(EBX, 2);
+    Instruction instruction = new DivInstruction(null, EAX, EBX);
+    instruction.execute(machine);
+    Assertions.assertEquals(0, machine.getRegisters().get(EAX));
+  }
+
+  @Test
+  void executeValidThree() {
+    registers.set(EAX, -4);
+    registers.set(EBX, 2);
+    Instruction instruction = new DivInstruction(null, EAX, EBX);
+    instruction.execute(machine);
+    Assertions.assertEquals(-2, machine.getRegisters().get(EAX));
+  }
+
+}
