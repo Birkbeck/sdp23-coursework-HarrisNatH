e554d78b59504bc8886aead432ed06116f70568c
Harris <harris.hendra@gmail.com>
2023-02-20 21:47:37 +0000

    SubInstruction class and test

--- src/sml/instruction/SubInstruction.java
+++ src/sml/instruction/SubInstruction.java
@@ -7,16 +7,25 @@ import sml.RegisterName;
 // TODO: write a JavaDoc for the class
 
 /**
- * @author
+ * A subclass for Instruction class, specialized for substraction operation
+ * In addition, this subclass has result and source variables
+ * @param SubInstruction() takes label, result, and source
+ * 			returns instantiated variables
+ * @param execute() abstract method inherited from Parent class (Instruction class)
+ * 			takes both result and source, execute a program
+ * 			returns result and substracted values (result - source)
+ * @param toString() another abstract method inherited from Parent class
+ * 			returns "" or : based on getLabel + opcode + result + source
+ * @author Harris
  */
 
-public class AddInstruction extends Instruction {
+public class SubInstruction extends Instruction {
 	private final RegisterName result;
 	private final RegisterName source;
 
-	public static final String OP_CODE = "add";
+	public static final String OP_CODE = "sub";
 
-	public AddInstruction(String label, RegisterName result, RegisterName source) {
+	public SubInstruction(String label, RegisterName result, RegisterName source) {
 		super(label, OP_CODE);
 		this.result = result;
 		this.source = source;
@@ -26,7 +35,7 @@ public class AddInstruction extends Instruction {
 	public int execute(Machine m) {
 		int value1 = m.getRegisters().get(result);
 		int value2 = m.getRegisters().get(source);
-		m.getRegisters().set(result, value1 + value2);
+		m.getRegisters().set(result, value1 - value2);
 		return NORMAL_PROGRAM_COUNTER_UPDATE;
 	}
 
